d4d75483619ba8f174dd601dc723276a6314ab66
nfreund22 <32681397+nfreund22@users.noreply.github.com>
2019-11-21 10:13:54 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 function init(){
 //add your javascrip between these two lines of code
 
+  document.getElementById("entrybutton").addEventListener("click", function() {
+    alert("Natalie Freund: " + document.getElementById("entryinput").value);
+    document.getElementById("entryoutput").innerHTML = document.getElementById("entryinput").value;
+  }, true);
 
 
 
