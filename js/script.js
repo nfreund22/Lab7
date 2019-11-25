81c15322e0215aa81c66b54817320dd2489d3881
nfreund22 <32681397+nfreund22@users.noreply.github.com>
2019-11-25 15:26:20 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 function init(){
 //add your javascrip between these two lines of code
   
-  function myFunction() {
+  
+  document.getElementById("entrybutton").addEventListener("click", function() {
     alert ("Natalie Freund: " + document.getElementById("entryinput").value);
     document.getElementById("textoutput").innerHTML = document.getElementById("entryinput").value;
-  }
-  
-  document.getElementById("entrybutton").addEventListener("click", myFunction());
+  });
 
 }
 window.addEventListener('load', init);
